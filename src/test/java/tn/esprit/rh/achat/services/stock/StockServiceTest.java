d7ba4188dede1c6f6be2a0625a0ce62867eacbe6
Nour9911 <nour.m'rad@esprit.tn>
2022-10-16 17:57:47 +0200

    mockito tests working

--- src/test/java/tn/esprit/rh/achat/services/stock/StockServiceTest.java
+++ src/test/java/tn/esprit/rh/achat/services/stock/StockServiceTest.java
@@ -1,20 +1,26 @@
 package tn.esprit.rh.achat.services.stock;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 import tn.esprit.rh.achat.entities.Stock;
 import tn.esprit.rh.achat.repositories.StockRepository;
+import tn.esprit.rh.achat.services.IStockService;
 import tn.esprit.rh.achat.services.StockServiceImpl;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -22,95 +28,71 @@ import static org.mockito.Mockito.when;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 
 @SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @ExtendWith(MockitoExtension.class)
 public class StockServiceTest {
 	
-	@Mock
-	StockRepository stockRepository;
-	
-	@InjectMocks
-	StockServiceImpl stockService;
-	
 
-	
-	
-	@Test
-	public void testRetrieveStock() {
-		
-		Stock stock = new Stock("stock1", 100, 1);
-		stock.setIdStock(1L);
-		
-	Mockito.when(stockRepository.findById(1L)).thenReturn(Optional.of(stock));
-	stockService.retrieveStock(1L);
-	Assertions.assertNotNull(stock);
-	
-	System.out.println(stock); 
-	System.out.println(" Retrieve is working correctly...!!");  
-	
-	}
-	
-	
-	@Test
-	public void createStockTest()
-	{
+@MockBean
+private StockRepository sr;
 
-		Stock stock2 = new Stock("stock2", 200, 2);
-		stock2.setIdStock(2L);
-		
-		stockService.addStock(stock2);
-		verify(stockRepository, times(1)).save(stock2);
-		System.out.println(stock2); 
-		System.out.println(" Create is working correctly...!!");  
-	}
-	
-	
-	
-	@Test
-	public void getAllStocksTest()
-	{
-		List<Stock> Stocklist = new ArrayList<Stock>() {
+private Stock stock1 = new Stock(1L,"stock1", 100, 1,null);
+private Stock stock2 = new Stock(7L,"stock2", 200, 2,null);
 
-			{
-		add(new Stock("stock3", 300, 3));
-		add(new Stock("stock4", 400, 4));
-		add(new Stock("stock5", 500, 5));
-			}};
-			
-			
-		when(stockService.retrieveAllStocks()).thenReturn(Stocklist);
-		//test
-		List<Stock> stList = stockService.retrieveAllStocks();
-		assertEquals(3, stList.size());
-		System.out.println(" Retrieve all is working correctly...!!");  
-	
-	}
-	
+ 
+@Autowired
+ IStockService is;
 
-	
-	
-	@Test
-	public void TestDeleteStock(){
 
-	Stock stock1 = new Stock("stock7", 700, 7);
-	stock1.setIdStock(7L);
-	
-	Mockito.lenient().when(stockRepository.findById(stock1.getIdStock())).thenReturn(Optional.of(stock1));
+@Test
+public void addStockTest() {
+	when(sr.save(stock1)).thenReturn(stock1);
+	assertNotNull(stock1);
+	assertEquals(stock1, is.addStock(stock1)); 
+	System.out.println("add works !");
+}
 
-	stockService.deleteStock(7L);
-	verify(stockRepository).deleteById(stock1.getIdStock());
-	
-	System.out.println(stock1);
-	System.out.println(" Delete is working correctly...!!");  
-	}
-	
-	
-	
-	
-	
-	
+
+@Test 
+public void retrieveAllStocksTest() {
+	when(sr.findAll()).thenReturn(Stream
+			.of(stock1,stock2)
+			.collect(Collectors.toList()));
+	assertEquals(2,is.retrieveAllStocks().size());
+	System.out.println("Retrieve operators works !");
+}
+
+
+
+@Test
+public void DeleteStockTest() {
+	sr.save(stock1);
+	is.deleteStock(stock1.getIdStock());
+	verify(sr, times(1)).deleteById(stock1.getIdStock());
+	System.out.println("Delete works !");
 	
 }
+
+
+@Test 
+public void UpdateStockTest() {
+	when(sr.save(stock1)).thenReturn(stock1);
+	assertNotNull(stock1);
+	assertEquals(stock1, is.updateStock(stock1));
+	System.out.println("Update works !");
+}
+
+@Test
+public void retrieveStockTest() {
+	when(sr.findById(stock1.getIdStock())).thenReturn(Optional.of(stock1));
+	assertEquals(stock1, is.retrieveStock(stock1.getIdStock()));
+	System.out.println("Retrieve works !");
+}
+		
+}
+
