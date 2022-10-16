c0dcf41c2304ec254c8c16f71827b9c276b63259
firas999 <mohamedfiras.mhalla@esprit.tn>
2022-10-16 17:40:55 +0100

    junitTest

--- src/test/java/tn/esprit/rh/achat/services/produit/ProduitServiceTest.java
+++ src/test/java/tn/esprit/rh/achat/services/produit/ProduitServiceTest.java
@@ -1,6 +1,8 @@
 package tn.esprit.rh.achat.services;
 
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
