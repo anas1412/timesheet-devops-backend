698b9c1e0f76ae45fdb654e935727d3972f9fdbf
anas1412 <anasbassoumi@gmail.com>
2022-10-17 19:52:34 +0100

    facture test

--- src/test/java/tn/esprit/rh/achat/services/facture/FactureServiceTest.java
+++ src/test/java/tn/esprit/rh/achat/services/facture/FactureServiceTest.java
@@ -1,14 +1,18 @@
 package tn.esprit.rh.achat.services.facture;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
+import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import tn.esprit.rh.achat.entities.Facture;
 import tn.esprit.rh.achat.repositories.FactureRepository;
@@ -25,14 +29,18 @@ import java.util.List;
 import java.util.Optional;
 
 @SpringBootTest
+@RunWith(SpringRunner.class)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @ExtendWith(MockitoExtension.class)
 public class FactureServiceTest {
 	
-	@Mock
+	@MockBean
 	FactureRepository factureRepository;
 	
-	@InjectMocks
+	@Autowired
 	FactureServiceImpl factureService;
+	
+	
 	
 	@Test
 	public void testRetrieveFacture() {
